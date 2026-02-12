6c2d849a2bda46a10dbd43826306457631066f82
macio <macio@homeserver.homelab>
2026-02-12 11:51:04 -0500

    fix(infra): update AWS region to eu-central-1

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket         = "ecobid-terraform-state"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"
     encrypt        = true
     dynamodb_table = "ecobid-terraform-locks"
   }
