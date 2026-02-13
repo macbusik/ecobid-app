8568860a94f24a66e434ecf8d1ef6231e350b1fd
macio <macio@homeserver.homelab>
2026-02-13 05:55:41 -0500

    refactor(infra): complete S3 module refactoring
    - Make S3 module fully reusable with generic variables
    - Update outputs to use generic naming
    - Add test bucket example in main.tf

--- infrastructure/terraform/modules/s3/main.tf
+++ infrastructure/terraform/modules/s3/main.tf
@@ -1,17 +1,19 @@
-resource "aws_s3_bucket" "images" {
-  bucket = "${var.project_name}-${var.environment}-images"
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = var.tags
 }
 
-resource "aws_s3_bucket_versioning" "images" {
-  bucket = aws_s3_bucket.images.id
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
 
   versioning_configuration {
-    status = "Enabled"
+    status = var.enable_versioning ? "Enabled" : "Disabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
-  bucket = aws_s3_bucket.images.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -20,36 +22,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "images" {
-  bucket = aws_s3_bucket.images.id
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "images" {
-  bucket = aws_s3_bucket.images.id
-
-  rule {
-    id     = "delete-old-images"
-    status = "Enabled"
-
-    expiration {
-      days = 90
-    }
-  }
-}
-
-resource "aws_s3_bucket_cors_configuration" "images" {
-  bucket = aws_s3_bucket.images.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "PUT", "POST"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
 }
--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,19 +1,9 @@
-output "dynamodb_table_name" {
-  description = "DynamoDB table name"
-  value       = module.dynamodb.table_name
+output "test_bucket_name" {
+  description = "Name of the test S3 bucket"
+  value       = module.test_bucket.bucket_name
 }
 
-output "s3_images_bucket" {
-  description = "S3 bucket for images"
-  value       = module.s3.images_bucket_name
-}
-
-output "cognito_user_pool_id" {
-  description = "Cognito User Pool ID"
-  value       = module.cognito.user_pool_id
-}
-
-output "api_gateway_url" {
-  description = "API Gateway URL"
-  value       = module.api_gateway.api_url
+output "test_bucket_arn" {
+  description = "ARN of the test S3 bucket"
+  value       = module.test_bucket.bucket_arn
 }
